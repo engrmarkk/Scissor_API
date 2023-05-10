e577a7aa1fd5ef66da5bb7278db4c389764af3e9
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-10 18:31:05 +0100

    the authentications

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -4,10 +4,10 @@ from flask.views import MethodView
 from ..schemas import UserSchema, LoginSchema
 from flask_jwt_extended import create_access_token, \
     create_refresh_token
+from ..utils import check_if_email_is_unique, check_if_username_is_unique
 from passlib.hash import pbkdf2_sha256 as sha256
 
-bp = Blueprint('auth', __name__, url_prefix='/users',
-               description='Operations on users')
+bp = Blueprint('auth', __name__, description='Operations on users')
 
 
 @bp.route('/register')
@@ -16,17 +16,9 @@ class RegisterUserResource(MethodView):
     @bp.response(201, UserSchema)
     def post(self, new_user):
         """Register a new user"""
-
-        # convert the username and email to lowercase
-        new_user.username = new_user.username.lower()
-        new_user.email = new_user.email.lower()
-
-        user = User.query.filter_by(username=new_user.username).first()
-        if user:
-            abort(409, message='Username already exists')
-        user = User.query.filter_by(email=new_user.email).first()
-        if user:
-            abort(409, message='Email already exists')
+        # check if the username and email are unique
+        check_if_username_is_unique(new_user.username.lower())
+        check_if_email_is_unique(new_user.email.lower())
         new_user.password = sha256.hash(new_user.password)
         new_user.save()
         return new_user
@@ -37,11 +29,7 @@ class LoginUserResource(MethodView):
     @bp.arguments(LoginSchema)
     def post(self, user):
         """Login a user"""
-
-        # convert the email to lowercase
-        user.email = user.email.lower()
-
-        current_user = User.query.filter_by(email=user.email).first()
+        current_user = User.query.filter_by(email=user.email.lower()).first()
         if not current_user:
             abort(404, message='User not found')
         if not sha256.verify(user.password, current_user.password):
