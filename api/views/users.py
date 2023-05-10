ac91b55d37a583e5ed5f07fb66f7a6f5e11637d0
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-10 19:53:46 +0100

    get all users view

---
+++ api/views/users.py
@@ -0,0 +1,17 @@
+from ..models import User
+from flask_smorest import abort, Blueprint
+from flask_jwt_extended import jwt_required
+from flask.views import MethodView
+from ..schemas import UserSchema
+
+bp = Blueprint('users', __name__, description='Operations on users')
+
+
+@bp.route('/users')
+class UsersResource(MethodView):
+    @bp.response(200, UserSchema(many=True))
+    @jwt_required()
+    def get(self):
+        """Get all users"""
+        users = User.query.all()
+        return users
