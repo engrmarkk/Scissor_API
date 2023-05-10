292172d01405572e6b1cde2735f5a1435523f6d2
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-10 18:06:19 +0100

    initialized Api imported from flask_smorest

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from .extensions import db, migrate, jwt
+from .extensions import db, migrate, jwt, api
 from .config import AppConfig
 from flask import Flask
 
@@ -8,6 +8,7 @@ def create_app(config_object=AppConfig):
     app.config.from_object(config_object)
     db.init_app(app)
     migrate.init_app(app, db)
+    api.init_app(app)
     jwt.init_app(app)
 
     return app
