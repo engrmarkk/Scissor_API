b75363f833212c945437da41acae86993f2131bd
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-11 20:00:22 +0100

    Revoke model

---
+++ api/models/revoke.py
@@ -0,0 +1,17 @@
+from ..extensions import db
+
+
+class RevokedToken(db.Model):
+    __tablename__ = 'revoked_tokens'
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def is_jti_blacklisted(cls, jti):
+        query = cls.query.filter_by(jti=jti).first()
+        return bool(query)
