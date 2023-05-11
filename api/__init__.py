9fc3c26cae81b1672d97437c61b33db86d95aaa9
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-11 16:36:20 +0100

    cors and cache initialization

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,10 @@
-from .extensions import db, migrate, jwt, api
+from .extensions import db, migrate, jwt, api, cors, cache
 from .config import config_object
 from flask import Flask
 from .models import User, Link
 from .auth import bp as auth_bp
 from .views.users import bp as users_bp
+from .views.url import bp as url_bp
 
 
 def create_app(configure=config_object['appcon']):
@@ -12,10 +13,13 @@ def create_app(configure=config_object['appcon']):
     db.init_app(app)
     migrate.init_app(app, db)
     api.init_app(app)
+    cors.init_app(app)
+    cache.init_app(app)
     jwt.init_app(app)
 
     api.register_blueprint(auth_bp)
     api.register_blueprint(users_bp)
+    api.register_blueprint(url_bp)
 
     @app.shell_context_processor
     def make_shell_context():
