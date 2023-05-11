e7b38ea3d7dc148a5f4bae6a97cb140839aa0179
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-11 16:48:16 +0100

    links schema

---
+++ api/schemas/link.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema, fields
+
+
+class LinkSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
+    url = fields.String(required=True)
+    short_url = fields.String(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+
+
+class GetLinks(Schema):
+    id = fields.Integer()
+    user_id = fields.Integer()
+    url = fields.String()
+    short_url = fields.String()
+    visit = fields.Integer()
+    created_at = fields.String()
