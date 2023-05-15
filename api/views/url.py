a2fe0f7757f3f5e2878be74a14e27606d1a63452
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-15 19:32:34 +0100

    url reformtted

--- api/views/url.py
+++ api/views/url.py
@@ -5,14 +5,15 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask.views import MethodView
 from ..schemas import LinkSchema, GetLinks
 from ..utils.url_validate import validate_url
+
 # from ..utils import check_if_user_is_still_logged_in
 from ..extensions import db, cache
 
 
-bp = Blueprint('urls', __name__, description='Operations on urls')
+bp = Blueprint("urls", __name__, description="Operations on urls")
 
 
-@bp.route('/short-urls')
+@bp.route("/short-urls")
 class CreateShortUrl(MethodView):
     @bp.arguments(LinkSchema)
     # @bp.response(201, LinkSchema)
@@ -20,18 +21,18 @@ class CreateShortUrl(MethodView):
     def post(self, new_url):
         """Create a new short url"""
         current_user = get_jwt_identity()
-        if not validate_url(new_url['url']):
-            abort(400, message='Invalid url')
+        if not validate_url(new_url["url"]):
+            abort(400, message="Invalid url")
         link = Link(**new_url, user_id=current_user)
         link.save()
         response = {
             "original url": link.url,
-            "shortened url": f"{request.host_url}{link.short_url}"
+            "shortened url": f"{request.host_url}{link.short_url}",
         }
         return response, 201
 
 
-@bp.route('/<short_url>')
+@bp.route("/<short_url>")
 class RedirectShortUrl(MethodView):
     @bp.response(302)
     @cache.memoize(timeout=3600)
@@ -39,13 +40,13 @@ class RedirectShortUrl(MethodView):
         """Redirect to the original url"""
         link = Link.query.filter_by(short_url=short_url).first()
         if not link:
-            abort(404, message='Url not found')
+            abort(404, message="Url not found")
         link.visit += 1
         db.session.commit()
         return redirect(link.url)
 
 
-@bp.route('/<short_url>/qr-code')
+@bp.route("/<short_url>/qr-code")
 @jwt_required()
 @cache.cached(timeout=3600)
 def qr_code(short_url):
@@ -56,11 +57,11 @@ def qr_code(short_url):
         link.qr_code = link.generate_qr_code()
         link.save()
     response = make_response(link.qr_code)
-    response.headers.set('Content-Type', 'image/jpeg')
+    response.headers.set("Content-Type", "image/jpeg")
     return response
 
 
-@bp.route('/all-links')
+@bp.route("/all-links")
 @bp.response(200, GetLinks(many=True))
 @jwt_required()
 @cache.cached(timeout=3600)
