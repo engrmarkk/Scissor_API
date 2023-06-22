58b58a6362ea4fb8c0756cb8cd1a48d3eaa145c0
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-06-22 14:29:36 +0100

    runserver

--- app_config.py
+++ app_config.py
@@ -1,6 +1,10 @@
 from api import create_app
+from api.config import config_object
 
-app = create_app()
+
+app = create_app(configure=config_object['prodcon'])
+
+# app = create_app()
 
 if __name__ == '__main__':
     app.run()
