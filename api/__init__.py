c90b26ab6a495f4796e9f54197792f6753642786
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-06-24 20:37:56 +0100

    update

--- api/__init__.py
+++ api/__init__.py
@@ -33,6 +33,7 @@ def create_app(configure=config_object['appcon']):
         if request.method == 'OPTIONS':
             headers = {
                 'Access-Control-Allow-Origin': 'http://localhost:3000',  # Replace with your frontend origin
+                'Access-Control-Allow-Origin': ' https://scissor-alpha.vercel.app',  # Replace with your frontend origin
                 'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
                 'Access-Control-Allow-Credentials': 'true'
