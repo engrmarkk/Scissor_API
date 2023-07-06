729951ab74defbb32efedb82f27318e2744065e9
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-06 09:44:16 +0100

    limiter extension

--- api/extensions/__init__.py
+++ api/extensions/__init__.py
@@ -4,6 +4,9 @@ from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 from flask_cors import CORS
 from flask_caching import Cache
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,3 +14,8 @@ jwt = JWTManager()
 api = Api()
 cors = CORS()
 cache = Cache()
+limiter = Limiter(get_remote_address,
+                  storage_uri=os.getenv("REDIS_URL"),
+                  storage_options={"socket_connect_timeout": 30},
+                  strategy="fixed-window",  # or "moving-window"
+                  )
