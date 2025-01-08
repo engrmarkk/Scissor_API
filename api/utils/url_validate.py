a4f77212b970c21325930f53aa5d090782f59247
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-01-08 13:35:04 +0100

    Update url_validate.py

--- api/utils/url_validate.py
+++ api/utils/url_validate.py
@@ -1,12 +1,14 @@
 from urllib import request
 from urllib.error import HTTPError, URLError
+import ssl
 
 
 def validate_url(url):
+    context = ssl._create_unverified_context()
     if not url.startswith('http://') and not url.startswith('https://'):
         url = 'http://' + url
     try:
-        request.urlopen(url)
+        request.urlopen(url, context=context)
         return True
     except (HTTPError, URLError):
         return False
